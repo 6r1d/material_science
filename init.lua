6f13b18f7f1a1fde1cea382e9dcf37082c4e6d3f
Victor Gridnevsky <vic.6r1d@gmail.com>
2019-07-26 04:30:39 +0300

    Adds settings support and some debugged blocks

--- init.lua
+++ init.lua
@@ -1,9 +1,20 @@
+material_science = {}
+
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 
+-- Settings
+dofile (modpath .. "/settings.lua")
+-- Materials
 dofile (modpath .. "/materials.lua")
+-- Recipes
 dofile (modpath .. "/recipes.lua")
+-- Machines
 dofile (modpath .. "/blocks/decomposer.lua")
+dofile (modpath .. "/blocks/coater.lua")
+dofile (modpath .. "/blocks/flame_fusion_injector.lua")
+dofile (modpath .. "/blocks/water_splitter.lua")
+-- Blocks
 dofile (modpath .. "/blocks/quartz_glass.lua")
 dofile (modpath .. "/blocks/phosphorus.lua")
 dofile (modpath .. "/blocks/aluminium_block.lua")
